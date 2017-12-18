1b4abc4aa2f2ec50dcd9f5638f66992de3b19171
Mantas Masalis <mantasmasalis@gmail.com>
2017-12-18 13:12:47 +0200

    Friendly Obstacles

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -21,6 +21,23 @@ public class Rocket : MonoBehaviour {
         Rotate();
 	}
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                print("OK");
+                break;
+            case "Fuel":
+                print ("Fuel");
+                break;
+            default:
+                print("dead");
+                break;
+        }
+    }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
